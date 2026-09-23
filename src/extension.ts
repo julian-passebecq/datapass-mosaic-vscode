61b86df8ff22f55f7cdebef10f4ce71e5fec2301
Julian Passebecq <64646018+julian-passebecq@users.noreply.github.com>
2026-09-23 14:37:57 +0200

    Route all modules through Workbench panel

--- src/extension.ts
+++ src/extension.ts
@@ -1,12 +1,23 @@
 import * as vscode from "vscode";
 import { LabTreeProvider } from "./labTree";
 import { MODULES } from "./modules";
-import { openModulePanel } from "./modulePanel";
+import { RuntimeManager } from "./runtimeManager";
+import { WorkbenchPanel } from "./workbenchPanel";
 
 export function activate(context: vscode.ExtensionContext): void {
-  context.subscriptions.push(vscode.window.registerTreeDataProvider("datapass.labs", new LabTreeProvider()));
+  const runtimeManager = new RuntimeManager(context.extensionUri);
+
+  context.subscriptions.push(
+    runtimeManager,
+    vscode.window.registerTreeDataProvider("datapass.labs", new LabTreeProvider())
+  );
+
   for (const module of MODULES) {
-    context.subscriptions.push(vscode.commands.registerCommand(module.command, () => openModulePanel(context, module)));
+    context.subscriptions.push(
+      vscode.commands.registerCommand(module.command, () => {
+        void WorkbenchPanel.show(context, runtimeManager, module.id);
+      })
+    );
   }
 }
 
